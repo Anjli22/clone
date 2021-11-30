6d642a98acba5946508dca415bdb084d96447278
Anjli22 <kushwahanjali22@gmail.com>
2021-11-30 19:15:52 +0530

    push

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,16 @@ import './Card.css';
 const Card = () => {
     return (
         <div class="card">
-            <div>
+            <div class="card-content">
                 <h1>one page site</h1>
                 <h2>lorem ispum is simple text</h2>
                 <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." </p>
                 <button class="btn">LEARN MORE</button>
             </div>
             <div >
-               <img class="image" src="https://www.youloveit.com/uploads/posts/2020-01/1579295763_youloveit_com_frozen_2_official_hd_art_big_images01.jpg"></img>
+               <img  class="image" src="https://t4.ftcdn.net/jpg/02/48/17/51/240_F_248175189_9j4bZw7WwkttDumepYd7hceSul8742X3.jpg" alt="my pic">
+
+               </img>
             </div>
             
         </div>
